6a397a60e01c33715dfd199ffbd012ec79146c6d
vrahul1997 <vijayvenu1997@gmail.com>
2021-05-06 05:28:24 -0300

    mongo done

--- common/db.py
+++ common/db.py
@@ -3,6 +3,7 @@ import os
 
 # client = MongoClient(os.environ.get("MONGO_CLIENT"))
 client = MongoClient(
-    "mongodb+srv://rahulll:N20FBJ7BHPAorXS8@cluster0.ucpg9.mongodb.net/test?authSource=admin&replicaSet=atlas-11wr07-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+    "mongodb+srv://rahulll:N20FBJ7BHPAorXS8@cluster0.ucpg9.mongodb.net/flask_test?retryWrites=true&w=majority"
 )
+
 FLASK_TEST_DB = client["flask_test"]
