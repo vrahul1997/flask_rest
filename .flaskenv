3239913fe71159db30255fa203c78f1631515bdd
vrahul1997 <vijayvenu1997@gmail.com>
2021-05-06 04:29:39 -0300

    almost done

--- .flaskenv
+++ .flaskenv
@@ -1,5 +1,5 @@
 FLASK_APP=app
 FLASK_ENV=development
 FLASK_RUN_PORT=2002
-MONGO_CLIENT=mongodb://localhost:27017
+MONGO_CLIENT=mongodb+srv://rahulll:N20FBJ7BHPAorXS8@cluster0.ucpg9.mongodb.net/test?authSource=admin&replicaSet=atlas-11wr07-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
 SECRET_KEY=puneetha
--- common/db.py
+++ common/db.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
 import os
 
-client = MongoClient(os.environ.get("MONGO_CLIENT"))
+# client = MongoClient(os.environ.get("MONGO_CLIENT"))
+client = MongoClient(
+    "mongodb+srv://rahulll:N20FBJ7BHPAorXS8@cluster0.ucpg9.mongodb.net/test?authSource=admin&replicaSet=atlas-11wr07-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+)
 FLASK_TEST_DB = client["flask_test"]
