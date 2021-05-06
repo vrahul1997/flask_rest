4859962ba7afdd5be93db93def09c7c52f8560f7
vrahul1997 <vijayvenu1997@gmail.com>
2021-05-06 05:18:27 -0300

    mongo done

--- common/db.py
+++ common/db.py
@@ -3,6 +3,6 @@ import os
 
 # client = MongoClient(os.environ.get("MONGO_CLIENT"))
 client = MongoClient(
-    "mongodb+srv://rahulll:@cluster0.ucpg9.mongodb.net/flask_test?retryWrites=true&w=majority"
+    "mongodb+srv://rahulll:Ra_Pun_zel@2002@cluster0.ucpg9.mongodb.net/flask_test?retryWrites=true&w=majority"
 )
 FLASK_TEST_DB = client["flask_test"]
